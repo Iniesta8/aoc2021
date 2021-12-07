00ae1dfdb4a31a0f77ab63e5725092f7efb06b30
Andreas Schnebinger <andi.schnebinger@googlemail.com>
2021-12-07 08:38:28 +0100

    Add solution for part2 that works for any input (not only mine :))

--- day07/day07.py
+++ day07/day07.py
@@ -1,30 +1,35 @@
 #!/usr/bin/env python3
 
-
 def parse_input():
     with open("./input") as f:
         return [(int(c)) for c in f.read().strip().split(",")]
 
 
 def part1(init_pos):
-    init_pos.sort()
     median = init_pos[len(init_pos)//2]
 
     return sum(abs(c - median) for c in init_pos)
 
 
 def part2(init_pos):
-    mean = sum(init_pos)//len(init_pos)
-
     def calc_costs(src, dst):
         dist = abs(dst - src)
-        return sum(range(dist + 1))
+        return dist * (dist + 1) // 2
 
-    return sum(calc_costs(c, mean) for c in init_pos)
+    best = 1e9
+    for t in range(init_pos[0], init_pos[-1] + 1):
+        costs = 0
+        for c in init_pos:
+            costs += calc_costs(c, t)
+        if costs < best:
+            best = costs
+
+    return best
 
 
 if __name__ == "__main__":
     init_pos = parse_input()
+    init_pos.sort()
 
     print(f"part1: {part1(init_pos)}")
     print(f"part2: {part2(init_pos)}")
