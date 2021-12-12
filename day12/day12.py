e732b4d9ca083b372b0ffe30d69c72b3d9f57a1b
Andreas Schnebinger <andi.schnebinger@googlemail.com>
2021-12-12 15:20:59 +0100

    Refactor solution for day12 (without recursion)

--- day12/day12.py
+++ day12/day12.py
@@ -5,46 +5,40 @@ from collections import defaultdict
 
 
 def parse_data():
-    graph = defaultdict(set)
+    graph = defaultdict(list)
 
     with open("./input") as f:
         for line in f.readlines():
             src, dst = line.strip().split("-")
-            graph[src].add(dst)
-            graph[dst].add(src)
+            graph[src].append(dst)
+            graph[dst].append(src)
 
         return graph
 
 
 def solve(graph, p2):
-    def dfs(graph, path, visited_caves, next_cave,
-            possible_paths, p2, twice_small=""):
-        if next_cave == "end":
-            path.append(next_cave)
-            visited_caves.add(next_cave)
-            possible_paths.append(path)
-            return
-        if next_cave in visited_caves and next_cave.islower():
-            if p2:
-                if next_cave == "start":
-                    return
-                if twice_small == "":
-                    twice_small = next_cave
+    sum = 0
+    stack = list([("start", set(["start"]), "")])
+
+    while stack:
+        cave, visited_smalls, twice_small = stack.pop()
+        if cave == "end":
+            sum += 1
+            continue
+        for next in graph[cave]:
+            if next not in visited_smalls:
+                new_smalls = set(visited_smalls)
+                if next.islower():
+                    new_smalls.add(next)
+                stack.append((next, new_smalls, twice_small))
+            elif next in visited_smalls:
+                if p2:
+                    if next not in ["start", "end"] and twice_small == "":
+                        stack.append((next, visited_smalls, next))
                 else:
-                    return
-            else:
-                return
+                    continue
 
-        path.append(next_cave)
-        visited_caves.add(next_cave)
-        for next in graph[next_cave]:
-            dfs(graph, list(path), set(visited_caves),
-                next, possible_paths, p2, twice_small)
-
-    possible_paths = []
-    dfs(graph, [],  set(), "start", possible_paths, p2)
-
-    return len(possible_paths)
+    return sum
 
 
 if __name__ == "__main__":
