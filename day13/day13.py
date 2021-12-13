14f409dd84b043fe914389d9dd11dbf2304d8883
Andreas Schnebinger <andi.schnebinger@googlemail.com>
2021-12-13 09:55:25 +0100

    Add solution for day13

---
+++ day13/day13.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+
+def parse_data():
+    with open("./input") as f:
+        raw_points, raw_folds = f.read().split("\n\n")
+
+        points = set()
+        for p in raw_points.split("\n"):
+            x, y = p.strip().split(",")
+            points.add((int(x), int(y)))
+
+        folds = []
+        for f in raw_folds.strip().split("\n"):
+            fold_along = f.strip().split()[-1]
+            t, pos = fold_along.split("=")
+            folds.append((t, int(pos)))
+
+        return points, folds
+
+
+def find_pos_after_fold(point, fold):
+    line = fold[1]
+    if fold[0] == "x":  # fold left
+        x, y = point
+        if x < line:
+            return point
+        else:
+            d = x - 1 - line
+            xi = line - d - 1
+            return xi, y
+    else:  # fold up
+        x, y = point
+        if y < line:
+            return point
+        else:
+            d = y - 1 - line
+            yi = line - d - 1
+            return x, yi
+
+
+def do_fold(points, fold):
+    new_points = set()
+    for p in points:
+        np = find_pos_after_fold(p, fold)
+        new_points.add(np)
+
+    return new_points
+
+
+def print_points(points):
+    max_x = max(points, key=lambda item: item[0])[0]
+    max_y = max(points, key=lambda item: item[1])[1]
+
+    for y in range(max_y + 1):
+        for x in range(max_x + 1):
+            if (x, y) in points:
+                print('#', end='')
+            else:
+                print('.', end='')
+        print()
+
+
+def part1(points, folds):
+    return len(do_fold(points, folds[0]))
+
+
+def part2(points, folds):
+    new_points = points
+    for f in folds:
+        new_points = do_fold(new_points, f)
+
+    print_points(new_points)
+
+
+if __name__ == "__main__":
+    points, folds = parse_data()
+
+    print(f"part1: {part1(points, folds)}")
+    print(f"part2:")
+    part2(points, folds)
