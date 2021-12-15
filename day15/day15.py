a6cbba65f5d0dbc9c8a1d8b0a336f973cee33257
Andreas Schnebinger <andi.schnebinger@googlemail.com>
2021-12-15 11:04:57 +0100

    Add solution for day15

---
+++ day15/day15.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import heapq
+
+adj_dirs = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+
+
+def parse_input():
+    with open("./input") as f:
+        grid = [[int(level) for level in line.strip()]
+                for line in f.readlines()]
+
+        return grid
+
+
+def expand_grid(grid, n):
+    rows = len(grid)
+    cols = len(grid[0])
+    new_grid = [[0 for _ in range(n * cols)]
+                for _ in range(n * rows)]
+
+    for r in range(len(new_grid)):
+        for c in range(len(new_grid[0])):
+            val = grid[r % rows][c % cols] + r // rows + c // cols
+            while val > 9:
+                val -= 9
+            new_grid[r][c] += val
+
+    return new_grid
+
+
+def solve(grid, n):
+    if n > 1:
+        grid = expand_grid(grid, n)
+
+    rows = len(grid)
+    cols = len(grid[0])
+
+    acc_risks = [[None for _ in range(cols)]
+                 for _ in range(rows)]
+    queue = [(0, (0, 0))]
+
+    while queue:
+        acc_risk, (r, c) = heapq.heappop(queue)
+
+        if r < 0 or r >= rows or c < 0 or c >= cols:
+            continue
+
+        new_risk = acc_risk + grid[r][c]
+
+        if acc_risks[r][c] is None or new_risk < acc_risks[r][c]:
+            acc_risks[r][c] = new_risk
+        else:
+            continue
+
+        if (r, c) == (rows - 1,  cols - 1):
+            # distination reached
+            break
+
+        for adj in adj_dirs:
+            rr, cc = r + adj[0], c + adj[1]
+            heapq.heappush(queue, (acc_risks[r][c], (rr, cc)))
+
+    return acc_risks[rows - 1][cols - 1] - grid[0][0]
+
+
+if __name__ == "__main__":
+    grid = parse_input()
+
+    print(f"part1: {solve(grid, 1)}")
+    print(f"part2: {solve(grid, 5)}")
