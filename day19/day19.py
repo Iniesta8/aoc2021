9edcce23ebafa11954a70a9be6dd19b805ae0e70
Andreas Schnebinger <andi.schnebinger@googlemail.com>
2021-12-20 18:05:12 +0100

    Refactor day19

--- day19/day19.py
+++ day19/day19.py
@@ -3,6 +3,7 @@
 
 from dataclasses import dataclass, field
 from itertools import combinations
+from functools import lru_cache
 
 
 def parse_input():
@@ -25,6 +26,7 @@ class Scanner:
     num: int
     pos: tuple() = None
     beacons: set() = field(default_factory=set)
+    directions: dict() = None
 
     def add_beacon(self, new_beacon):
         self.beacons.add(new_beacon)
@@ -37,8 +39,7 @@ class Scanner:
         return str(self)
 
     def __hash__(self):
-        return hash(self.num) + hash(sum([hash(b) for b in self.beacons])) \
-            + hash(self.pos)
+        return hash(self.num)
 
     def rotate_x(self):
         s = Scanner(self.num)
@@ -61,65 +62,60 @@ class Scanner:
             s.add_beacon((y, -x, z))
         return s
 
+    def get_directions(self):
+        if self.directions:
+            return self.directions
+        dirs = {}
+        for ba in self.beacons:
+            for bb in self.beacons:
+                if ba == bb:
+                    continue
+                dirs[calc_direction(ba, bb)] = ba
+        self.directions = dirs
+        return dirs
 
-def calc_manhattan_distance(a, b):
-    return sum(abs(ai - bi) for ai, bi, in zip(a, b))
+    @lru_cache
+    def get_orientations(self):
+        scanner_orientations = set()
+
+        scanner_orientations.add(self)
+        old_length = len(scanner_orientations)
+        length = 0
+
+        while True:
+            for ori in set(scanner_orientations):
+                scanner_orientations.add(ori.rotate_x())
+                scanner_orientations.add(ori.rotate_y())
+                scanner_orientations.add(ori.rotate_z())
+
+            length = len(scanner_orientations)
+            if length == old_length:
+                break
+
+            old_length = length
+
+        return scanner_orientations
+
+    def get_common_beacons(self, other):
+        for ori in other.get_orientations():
+            dirs_a = self.get_directions()
+            dirs_b = ori.get_directions()
+
+            common_beacons = set()
+            for dir_a, ba in dirs_a.items():
+                if dir_a in dirs_b:
+                    common_beacons.add((ba, dirs_b[dir_a]))
+
+            if len(common_beacons) >= 12:
+                return (ori, common_beacons)
+
+    def calc_manhattan_distance(self, other):
+        return sum(abs(ai - bi) for ai, bi, in zip(self.pos, other.pos))
 
 
+@lru_cache
 def calc_direction(a, b):
     return (b[0] - a[0], b[1] - a[1], b[2] - a[2])
-
-
-def get_directions(scanner: Scanner):
-    dirs = {}
-    for ba in scanner.beacons:
-        for bb in scanner.beacons:
-            if ba == bb:
-                continue
-            dirs[calc_direction(ba, bb)] = ba
-
-    return dirs
-
-
-def get_orientations(scanner: Scanner):
-    scanner_orientations = set()
-
-    scanner_orientations.add(scanner)
-    old_length = len(scanner_orientations)
-    length = 0
-
-    while True:
-        for ori in set(scanner_orientations):
-            scanner_orientations.add(ori.rotate_x())
-            scanner_orientations.add(ori.rotate_y())
-            scanner_orientations.add(ori.rotate_z())
-
-        length = len(scanner_orientations)
-        if length == old_length:
-            break
-
-        old_length = length
-
-    return scanner_orientations
-
-
-def __common_beacons(a, b):
-    dirs_a = get_directions(a)
-    dirs_b = get_directions(b)
-
-    common_beacons = set()
-    for dir_a, ba in dirs_a.items():
-        if dir_a in dirs_b:
-            common_beacons.add((ba, dirs_b[dir_a]))
-
-    return common_beacons
-
-
-def get_common_beacons(fixed_scanner, scanner):
-    for ori in get_orientations(scanner):
-        common_beacons = __common_beacons(fixed_scanner, ori)
-        if len(common_beacons) >= 12:
-            return (ori, common_beacons)
 
 
 def solve(scanners):
@@ -128,41 +124,42 @@ def solve(scanners):
     not_fixed_scanners = scanners[1:]
 
     while len(not_fixed_scanners) > 0:
-        new_known_scanner = None
+        new_fixed_scanner = None
         remove_scanner = None
 
         for not_fixed_scanner in not_fixed_scanners:
             for fixed_scanner in fixed_scanners:
-                overlap_info = get_common_beacons(
-                    fixed_scanner, not_fixed_scanner)
+                overlap_info = fixed_scanner.get_common_beacons(
+                    not_fixed_scanner)
                 if overlap_info:
                     ori, common_beacons = overlap_info
                     print(
-                        f"Known scanner {fixed_scanner.num:>2} and scanner {not_fixed_scanner.num}")
+                        f"Scanner {fixed_scanner.num} and scanner {not_fixed_scanner.num} have overlapping beacons")
                     fixed_pos, new_pos = common_beacons.pop()
                     shift = calc_direction(fixed_pos, new_pos)
                     remove_scanner = not_fixed_scanner
-                    new_known_scanner = Scanner(not_fixed_scanner.num, shift)
-                    new_known_scanner.beacons = set(
+                    new_fixed_scanner = Scanner(not_fixed_scanner.num, shift)
+                    new_fixed_scanner.beacons = set(
                         [calc_direction(shift, b) for b in ori.beacons])
                     break
 
-        if new_known_scanner:
+        if new_fixed_scanner:
             print(
-                f"Position of scanner {new_known_scanner.num} detected: {new_known_scanner.pos}")
-            fixed_scanners.append(new_known_scanner)
+                f"Position of scanner {new_fixed_scanner.num} detected")
+            fixed_scanners.append(new_fixed_scanner)
         if remove_scanner:
             not_fixed_scanners.remove(remove_scanner)
 
+    print("\nFinal scanner positions:")
     beacons = set()
     for fixed_scanner in fixed_scanners:
         print(
-            f"Position of scanner {fixed_scanner.num} is {fixed_scanner.pos}")
+            f"Scanner {fixed_scanner.num:>2}: {fixed_scanner.pos}")
         beacons |= fixed_scanner.beacons
+    print()
 
-    max_distance = 0
-    for (a, b) in combinations(fixed_scanners, 2):
-        max_distance = max(max_distance, calc_manhattan_distance(a.pos, b.pos))
+    max_distance = max(a.calc_manhattan_distance(b)
+                       for (a, b) in combinations(fixed_scanners, 2))
 
     return len(beacons), max_distance
 
